3ef95c9051f87330c0070b26e52a7fc65aee4739
umakantmanu <umakant@manufactured.com>
2024-12-30 19:22:23 +0530

    Add tools, fastify server and some needed packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 // .eslintrc.js
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-        '@stylistic/ts'
-    ],
-    rules: {
-        '@stylistic/ts/indent': ['error', 2], // Using Stylistic's indent rule
-        // Add more stylistic rules as needed
-    },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    '@stylistic/ts'
+  ],
+  rules: {
+    '@stylistic/member-delimiter-style': ['error', { "multiline": { "delimiter": "comma" }, "singleline": { "delimiter": "comma" } }],
+    '@stylistic/ts/semi': ['error', 'always'],
+    '@stylistic/ts/indent': ['error', 2],
+    '@stylistic/ts/object-curly-newline': ['error', { "consistent": true }],
+    '@stylistic/js/arrow-parens': ['error', 'as-needed', { "requireForBlockBody": true }],
+    'require-await': 'error',
+  },
 };
