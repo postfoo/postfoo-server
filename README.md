1442effdf6bbea4efda71d1fd1bc5e8fff6267a7
umakantmanu <umakant@manufactured.com>
2025-01-02 01:54:26 +0530

    Add db connect, keys for local

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # postfoo-server
+
+### Setup
+
+- Start the Postgres Database in [Docker](https://www.docker.com/get-started):
+
+  ```sh
+  docker-compose up -d
+  ```
+
+- Run the first build & deploy the db:
+
+  ```sh
+  npm install
+  cp .env.example .env
+  npm run build
+  npm run db:deploy
+  ```
+
+- Run the server:
+
+  ```sh
+  npm run dev
+  ```
+
+### Environment Variables
+
+The repo has a file called `.env.example` for sample env variables. Copy the values from `.env.example` to `.env` and change the values as needed.
+
+Note: Generally for local development, you not need to change anything in the `.env` file.
+
+## Migrations
+
+### Creating a new migration
+
+- Make changes to [prisma/schema.prisma](prisma/schema.prisma)
+- Run `npm run db:migrate`
+- You can use `npm run db:migrate:dryrun` to just see the SQL before running it.
+
+## Deployment
+
+### Deploying to dev
+
+TODO
+
+### Deploying to prod
+
+TODO
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import 'module-alias/register'
 
+import cors from '@fastify/cors'
 import fastify from 'fastify'
+import db from 'src/db'
 import graphql from 'src/graphql'
 import logger from 'src/utils/logger'
 import pkg from 'src/utils/pkg'
@@ -14,9 +16,24 @@ app.get('/', (_req, res) => {
   res.send('OK')
 })
 
+app.get('/health', async (_req) => {
+  // health check the DB too
+  await db.$queryRawUnsafe('SELECT 1')
+  return {
+    statusCode: 200,
+    RELEASE_AT: process.env.RELEASE_AT || '',
+  }
+})
+
+app.register(cors, {
+  // Prod access only from main domain
+  origin: process.env.MODE === 'prod' ? [/\.postfoo\.com$/] : '*',
+})
+
 const main = async () => {
   try {
     const port = process.env.PORT ? Number(process.env.PORT) : 4000
+    await db.$connect()
 
     await graphql.setup(app)
     // 0.0.0.0 is the default host for Docker
@@ -32,6 +49,7 @@ const main = async () => {
 // Graceful shutdown
 process.on('beforeExit', () => {
   app.close()
+  db.$disconnect()
 })
 
 main()
