f1f82326a848a7125047366a304e35083ec46b4e
umakantmanu <umakant@manufactured.com>
2025-01-03 22:57:50 +0530

    Remove unused code

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,4 +1,3 @@
-import { fastifyRequestContext } from '@fastify/request-context'
 import { FastifyInstance } from 'fastify'
 import mercurius from 'mercurius'
 import { loadSchema } from 'src/graphql/schema'
@@ -9,48 +8,6 @@ import logger from 'src/utils/logger'
 
 async function setup(app: FastifyInstance) {
   const schema = await loadSchema()
-
-  app.register(fastifyRequestContext, {
-    hook: 'onRequest',
-    defaultStoreValues: {
-      token: null,
-      user: null,
-      jwt: null,
-    },
-  })
-
-  app.addHook('onRequest', (req, _reply, done) => {
-    const token = req.headers.authorization?.replace('Bearer ', '')
-    if (token) {
-      jwt.verify(token)
-        .then((jwt) => {
-          // If user is not found, the promise will throw an error
-          model.user.get(jwt.sub)
-            .then((user) => {
-              req.requestContext.set('user', user)
-              req.requestContext.set('token', token)
-              req.requestContext.set('jwt', jwt)
-            })
-            .catch((err) => {
-              logger.error(err)
-              req.requestContext.set('user', null)
-              req.requestContext.set('token', null)
-              req.requestContext.set('jwt', null)
-            })
-            .finally(() => {
-              done()
-            })
-        })
-        .catch((err) => {
-          logger.error(err)
-          req.requestContext.set('user', null)
-          req.requestContext.set('token', null)
-          req.requestContext.set('jwt', null)
-          done()
-        })
-    }
-    done()
-  })
 
   app.register(mercurius, {
     schema,
