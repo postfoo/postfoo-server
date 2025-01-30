139d673f18b2c87264bc618d8bf4b80c7e378ce3
umakantmanu <umakant@manufactured.com>
2025-01-30 16:47:04 +0530

    update error tracking

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -7,7 +7,6 @@ import { User } from 'src/types'
 import * as jwt from 'src/utils/jwt'
 import logger from 'src/utils/logger'
 import sentry from 'src/utils/sentry'
-import { delay } from 'src/utils/utils'
 
 const getContext = async (req: FastifyRequest) => {
   const token = req.headers.authorization?.replace('Bearer ', '')
@@ -30,29 +29,6 @@ const getContext = async (req: FastifyRequest) => {
   }
 }
 
-/*
-  app.addHook('preHandler', (req, _res) => {
-    sentry.withScope(async (scope) => {
-      const { user } = await getContext(req)
-      const gqlParams = req.body as { operationName?: string, variables?: Record<string, any>, query?: string }
-
-      scope.addEventProcessor(event => sentry.Handlers.parseRequest(event, req))
-
-      let sentryUser: SentryUser = { ip_address: req.ip }
-      if (user) {
-        sentryUser = { ...sentryUser, id: user.id, name: model.user.name(user), isSuperadmin: model.user.isSuperadmin(user) }
-      }
-      scope.setUser(sentryUser)
-
-      scope.setTags({ 'graphql.name': gqlParams?.operationName || 'none' })
-      scope.setExtra('query', gqlParams?.query)
-      if (gqlParams.variables) {
-        scope.setContext('variables', gqlParams.variables)
-      }
-    })
-  })
-  */
-
 async function setup(app: FastifyInstance) {
   const schema = await loadSchema()
 
@@ -61,47 +37,45 @@ async function setup(app: FastifyInstance) {
     graphiql: process.env.MODE !== 'prod' ? true : false,
     path: '/api',
     context: getContext,
+    errorFormatter: (execution, ...args) => {
+      sentry.withScope(async (scope) => {
+        const req = args[0].reply.request
+        const { user } = await getContext(req)
+        const gqlParams = req.body as { operationName?: string, variables?: Record<string, any>, query?: string }
+
+        let sentryUser: SentryUser = { ip_address: req.ip }
+        if (user) {
+          sentryUser = { ...sentryUser, id: user.id, name: model.user.name(user), isSuperadmin: model.user.isSuperadmin(user) }
+        }
+        scope.setUser(sentryUser)
+
+        scope.setTags({ 'graphql.name': gqlParams?.operationName || 'none' })
+        scope.setExtra('query', gqlParams?.query)
+        if (gqlParams.variables) {
+          scope.setContext('variables', gqlParams.variables)
+        }
+
+        if (execution.errors) {
+          for (const err of execution.errors) {
+            if (err.extensions) {
+              // These are input validation errors and we don't want to track them as we know
+              // many users will make mistakes in their input.
+              continue
+            }
+            if (err.path) {
+              scope.setTag('path', err.path.join('.'))
+            }
+            // scope.setTag('error.code', err.extensions?.code || err.name)
+            scope.setExtra('error.message', err.message)
+            sentry.captureException(err, scope)
+          }
+        }
+      })
+      return mercurius.defaultErrorFormatter(execution, ...args)
+    }
   })
 
   await app.ready()
-
-  app.graphql.addHook('onResolution', async (execution, context) => {
-    await delay(100)
-
-    sentry.withScope(async (scope) => {
-      const req = context.reply.request
-      const { user } = await getContext(req)
-      const gqlParams = req.body as { operationName?: string, variables?: Record<string, any>, query?: string }
-
-      let sentryUser: SentryUser = { ip_address: req.ip }
-      if (user) {
-        sentryUser = { ...sentryUser, id: user.id, name: model.user.name(user), isSuperadmin: model.user.isSuperadmin(user) }
-      }
-      scope.setUser(sentryUser)
-
-      scope.setTags({ 'graphql.name': gqlParams?.operationName || 'none' })
-      scope.setExtra('query', gqlParams?.query)
-      if (gqlParams.variables) {
-        scope.setContext('variables', gqlParams.variables)
-      }
-
-      if (execution.errors) {
-        for (const err of execution.errors) {
-          if (err.extensions) {
-            // These are input validation errors and we don't want to track them as we know
-            // many users will make mistakes in their input.
-            continue
-          }
-          if (err.path) {
-            scope.setTag('path', err.path.join('.'))
-          }
-          // scope.setTag('error.code', err.extensions?.code || err.name)
-          scope.setExtra('error.message', err.message)
-          sentry.captureException(err, scope)
-        }
-      }
-    })
-  })
 }
 
 export default { setup }
